a9ba085ad04f61be76535180214e54574e6eca48
Andy Wingo <wingo@igalia.com>
2025-04-16 12:43:15 +0200

    Add missing file

---
+++ src/embedder-api-impl.h
@@ -0,0 +1,12 @@
+#ifndef EMBEDDER_API_IMPL_H
+#define EMBEDDER_API_IMPL_H
+
+#include "gc-embedder-api.h"
+
+#ifndef GC_EMBEDDER
+#define GC_EMBEDDER "whippet-embedder.h"
+#endif
+
+#include GC_EMBEDDER
+
+#endif // EMBEDDER_API_IMPL_H
