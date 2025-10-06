48538abaca8a99149ce89498c65e99945ed0bcfd
Andy Wingo <wingo@igalia.com>
2025-10-06 13:26:08 +0200

    Switch gc-atomics to use generic builtins
    Too bad that C11 couldn't do this, but this is GCC- and
    Clang-compatible, which is enough.

--- api/gc-atomics.h
+++ api/gc-atomics.h
@@ -3,64 +3,73 @@
 
 #include <stdatomic.h>
 
-#define gc_atomic_cast(loc) \
-  _Generic((loc),                                                       \
-           uint8_t*   : ((_Atomic uint8_t*) (loc)),                     \
-           uint16_t*  : ((_Atomic uint16_t*) (loc)),                    \
-           uint32_t*  : ((_Atomic uint32_t*) (loc)),                    \
-           uint64_t*  : ((_Atomic uint64_t*) (loc)),                    \
-           int8_t*    : ((_Atomic int8_t*) (loc)),                      \
-           int16_t*   : ((_Atomic int16_t*) (loc)),                     \
-           int32_t*   : ((_Atomic int32_t*) (loc)),                     \
-           int64_t*   : ((_Atomic int64_t*) (loc)),                     \
-           void**     : ((_Atomic (void*)*) (loc)),                     \
-           default    : (loc))
+// This file defines some wrappers around C11 atomic operations.  There
+// are two goals:
+//
+//  1. Default to acquire/release memory ordering, and to make other
+//     memory orderings explicit.
+//
+//  2. Denote the access as atomic, not the object being accessed.  In
+//     practice this means that users of this interface don't often use
+//     the _Atomic type qualifier or the atomic_int types for the
+//     accessed objects.
+//
+// The second goal is gnarly.  GCC doesn't require _Atomic qualifiers,
+// which means gc_atomic_load(LOC) can directly translate to
+// atomic_load_explicit(LOC, memory_order_acquire), without regard to
+// the type of LOC.  But this is nonstandard.  To do things in a
+// standards-compliant way, we'd need to use _Generic maybe, but as far
+// as I can tell, there is no way to turn e.g. struct my_foo ** into
+// _Atomic(struct my_foo*)*.  (My kingdom for generics!)
+//
+// So instead we are going to bottom out to the __atomic intrinsics,
+// which are supported on GCC but also Clang.
 
 #define gc_atomic(verb, loc, ...) \
-  atomic_##verb(gc_atomic_cast(loc), ##__VA_ARGS__)
+  __atomic_##verb(loc, ##__VA_ARGS__)
 
 #define gc_atomic_load(loc)                                             \
-  gc_atomic(load_explicit, loc, memory_order_acquire)
+  gc_atomic(load_n, loc, __ATOMIC_ACQUIRE)
 #define gc_atomic_load_relaxed(loc)                                     \
-  gc_atomic(load_explicit, loc, memory_order_relaxed)
+  gc_atomic(load_n, loc, __ATOMIC_RELAXED)
 
 #define gc_atomic_store(loc, val)                                       \
-  gc_atomic(store_explicit, loc, val, memory_order_release)
+  gc_atomic(store_n, loc, val, __ATOMIC_RELEASE)
 #define gc_atomic_store_relaxed(loc, val)                               \
-  gc_atomic(store_explicit, loc, val, memory_order_relaxed)
+  gc_atomic(store_n, loc, val, __ATOMIC_RELAXED)
 
 #define gc_atomic_cmpxchg_weak(loc, expected, val)                      \
-  gc_atomic(compare_exchange_weak_explicit, loc, expected, val,         \
-            memory_order_acq_rel, memory_order_acquire)
+  gc_atomic(compare_exchange_n, loc, expected, val, 1,                  \
+            __ATOMIC_ACQ_REL, __ATOMIC_ACQUIRE)
 
 #define gc_atomic_cmpxchg_weak_seq_cst(loc, expected, val)              \
-  gc_atomic(compare_exchange_weak_explicit, loc, expected, val,         \
-            memory_order_seq_cst, memory_order_seq_cst)
+  gc_atomic(compare_exchange_n, loc, expected, val, 1,                  \
+            __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)
 
 #define gc_atomic_cmpxchg_strong(loc, expected, val)                    \
-  gc_atomic(compare_exchange_strong_explicit, loc, expected, val,       \
-            memory_order_acq_rel, memory_order_acquire)
+  gc_atomic(compare_exchange_n, loc, expected, val, 0,                  \
+            __ATOMIC_ACQ_REL, __ATOMIC_ACQUIRE)
 
 #define gc_atomic_cmpxchg_strong_seq_cst(loc, expected, val)            \
-  gc_atomic(compare_exchange_strong_explicit, loc, expected, val,       \
-            memory_order_seq_cst, memory_order_seq_cst)
+  gc_atomic(compare_exchange_n, loc, expected, val, 0,                  \
+            __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)
 
 #define gc_atomic_swap(loc, val)                                        \
-  gc_atomic(exchange_explicit, loc, val, memory_order_acq_rel)
+  gc_atomic(exchange_n, loc, val, __ATOMIC_ACQ_REL)
 
 #define gc_atomic_swap_relaxed(loc, val)                                \
-  gc_atomic(exchange_explicit, loc, val, memory_order_relaxed)
+  gc_atomic(exchange_n, loc, val, __ATOMIC_RELAXED)
 
 #define gc_atomic_fetch_add(loc, val)                                   \
-  gc_atomic(fetch_add_explicit, loc, val, memory_order_acq_rel)
+  gc_atomic(fetch_add, loc, val, __ATOMIC_ACQ_REL)
 
 #define gc_atomic_fetch_add_relaxed(loc, val)                           \
-  gc_atomic(fetch_add_explicit, loc, val, memory_order_relaxed)
+  gc_atomic(fetch_add, loc, val, __ATOMIC_RELAXED)
 
 #define gc_atomic_fetch_sub(loc, val)                                   \
-  gc_atomic(fetch_sub_explicit, loc, val, memory_order_acq_rel)
+  gc_atomic(fetch_sub, loc, val, __ATOMIC_ACQ_REL)
 
 #define gc_atomic_fetch_sub_relaxed(loc, val)                           \
-  gc_atomic(fetch_sub_explicit, loc, val, memory_order_relaxed)
+  gc_atomic(fetch_sub, loc, val, __ATOMIC_RELAXED)
 
 #endif // GC_ATOMICS_H
