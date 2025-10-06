7b147d357ba6e5b87e3e73b240defbb67d82fc36
Andy Wingo <wingo@igalia.com>
2025-10-06 09:53:00 +0200

    Add some missing parameter names for bdw-attrs.h

--- api/bdw-attrs.h
+++ api/bdw-attrs.h
@@ -47,14 +47,14 @@ static inline size_t gc_allocator_freelist_offset(size_t size,
 static inline size_t gc_allocator_alloc_table_alignment(void) {
   return 0;
 }
-static inline uint8_t gc_allocator_alloc_table_begin_pattern(enum gc_allocation_kind) {
+static inline uint8_t gc_allocator_alloc_table_begin_pattern(enum gc_allocation_kind kind) {
   GC_CRASH();
 }
 static inline uint8_t gc_allocator_alloc_table_end_pattern(void) {
   GC_CRASH();
 }
 
-static inline enum gc_old_generation_check_kind gc_old_generation_check_kind(size_t) {
+static inline enum gc_old_generation_check_kind gc_old_generation_check_kind(size_t obj_size) {
   return GC_OLD_GENERATION_CHECK_NONE;
 }
 static inline uint8_t gc_old_generation_check_alloc_table_tag_mask(void) {
@@ -64,7 +64,7 @@ static inline uint8_t gc_old_generation_check_alloc_table_young_tag(void) {
   GC_CRASH();
 }
 
-static inline enum gc_write_barrier_kind gc_write_barrier_kind(size_t) {
+static inline enum gc_write_barrier_kind gc_write_barrier_kind(size_t obj_size) {
   return GC_WRITE_BARRIER_NONE;
 }
 static inline size_t gc_write_barrier_field_table_alignment(void) {
